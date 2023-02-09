348e98a77495881d4c94f9d23a128f1f54eccb04
Deve-Tom <2314871946@qq.com>
2023-02-09 18:08:11 +0800

    加入了hertz模块

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
 
 func main() {
-	fmt.Print("Hello world")
+	h := server.Default()
+
+	h.GET("/ping", func(c context.Context, ctx *app.RequestContext) {
+		ctx.JSON(consts.StatusOK, utils.H{"message": "pong"})
+	})
+
+	h.Spin()
 }
